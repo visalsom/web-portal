f090619439969a61f6da4c0dd78d6dc195e3e0e9
visalsom <visalsom538@gmail.com>
2023-02-09 01:19:56 +0700

    final

--- webportal/settings.py
+++ webportal/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY =os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')=="True"
 
-ALLOWED_HOSTS = ['.vercel.app' ]
-CSRF_TRUSTED_ORIGINS= []
+ALLOWED_HOSTS = [ 'localhost','127.0.0.1','web-portal.fly.dev']
+CSRF_TRUSTED_ORIGINS= ['https://web-portal.fly.dev']
 
 
 # Application definition
