b1c7cde01a76f1d4c71724e4b2b62f5460de25f1
visalsom <visalsom538@gmail.com>
2023-02-09 20:46:32 +0700

    testinv v1.0.3

--- webportal/settings.py
+++ webportal/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY =os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')=="True"
 
-ALLOWED_HOSTS = [ 'localhost','127.0.0.1','*']
+ALLOWED_HOSTS = [ 'localhost','127.0.0.1','34.142.192.239','*']
 CSRF_TRUSTED_ORIGINS= []
 
 
